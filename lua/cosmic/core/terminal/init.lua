0d758652e59c27b2ea3c0d38bf390fec28411076
Matt Leong <mattleong91@gmail.com>
2021-10-22 18:58:12 -0700

    feat(terminal): use shell as title

--- lua/cosmic/core/terminal/init.lua
+++ lua/cosmic/core/terminal/init.lua
@@ -1,9 +1,10 @@
 local colors = require('cosmic.core.theme.colors')
 local highlight = require('cosmic.utils').highlight
 local g = vim.g
+local title = vim.env.SHELL
 
-g.floaterm_width = 0.8
+g.floaterm_width = 0.7
 g.floaterm_height = 0.8
-g.floaterm_title = '|👾 ($1/$2)|'
+g.floaterm_title = '[' .. title .. ']:($1/$2)'
 g.floaterm_opener = 'vsplit'
 highlight('FloatermBorder', 'None', colors.floatBorder)
