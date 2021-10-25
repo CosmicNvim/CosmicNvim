6306ed7293a7b8067f89ef02d2b3eb1644de67fd
Matt Leong <mattleong91@gmail.com>
2021-10-25 08:02:19 -0700

    feat(config): additional mappings config

--- lua/cosmic/init.lua
+++ lua/cosmic/init.lua
@@ -7,6 +7,13 @@ require('cosmic.editor')
 do
   local ok, err = pcall(require, 'cosmic.config.editor')
   if not ok then
-    error(string.format('Error loading custor editor settings...\n\n%s', err))
+    error(string.format('Error loading custom editor settings...\n\n%s', err))
+  end
+end
+
+do
+  local ok, err = pcall(require, 'cosmic.config.mappings')
+  if not ok then
+    error(string.format('Error loading custom mapping settings...\n\n%s', err))
   end
 end
