7b75433505758f851357e947390b775e13416688
Matt Leong <mattleong91@gmail.com>
2021-11-03 20:16:32 -0700

    feat(mapping): <leader>f{x} for basic nav mappings, <leader>s was...not great

--- lua/cosmic/core/navigation/mappings.lua
+++ lua/cosmic/core/navigation/mappings.lua
@@ -11,10 +11,10 @@ end
 
 M.init = function()
   -- navigation
-  map('n', '<leader>sf', '<cmd>lua require("cosmic.core.navigation.mappings").project_files()<cr>')
-  map('n', '<leader>sp', ':Telescope find_files<cr>')
-  map('n', '<leader>sk', ':Telescope buffers<cr>')
-  map('n', '<leader>ss', ':Telescope live_grep<cr>')
+  map('n', '<leader>ff', '<cmd>lua require("cosmic.core.navigation.mappings").project_files()<cr>')
+  map('n', '<leader>fp', ':Telescope find_files<cr>')
+  map('n', '<leader>fk', ':Telescope buffers<cr>')
+  map('n', '<leader>fs', ':Telescope live_grep<cr>')
 
   -- git navigation
   map('n', '<leader>ggc', ':Telescope git_commits<cr>')
