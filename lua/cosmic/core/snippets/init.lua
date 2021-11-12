e6804647039f5babe8a9c82fa3f52950c5f0ef3d
Matt Leong <mattleong91@gmail.com>
2021-11-12 10:14:13 -0800

    feat(snippets): lazy load snippets only

--- lua/cosmic/core/snippets/init.lua
+++ lua/cosmic/core/snippets/init.lua
@@ -24,8 +24,7 @@ ls.snippets = {
 ls.snippets.javascript = ls.snippets.html
 ls.snippets.javascriptreact = ls.snippets.html
 ls.snippets.typescriptreact = ls.snippets.html
-require('luasnip/loaders/from_vscode').load({ include = { 'html' } })
+-- require('luasnip/loaders/from_vscode').load({ include = { 'html' } })
 
 -- You can also use lazy loading so you only get in memory snippets of languages you use
-
-require('luasnip/loaders/from_vscode').lazy_load()
+require('luasnip/loaders/from_vscode').lazy_load({ include = { 'html' } })
