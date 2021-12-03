a3332a801f0bb07faa35e4b74b87663989cbc271
Matt Leong <mattleong91@gmail.com>
2021-12-03 14:41:04 -0800

    feat(utils): remove timeout for update success notification

--- lua/cosmic/utils/init.lua
+++ lua/cosmic/utils/init.lua
@@ -122,7 +122,7 @@ function M.update()
       end,
       on_exit = function()
         if vim.tbl_isempty(errors) then
-          Logger:log('Updated! Running CosmicReloadSync...', { timeout = 30000 })
+          Logger:log('Updated! Running CosmicReloadSync...')
           M.reload_user_config_sync()
         else
           table.insert(errors, 1, 'Something went wrong! Please pull changes manually.')
