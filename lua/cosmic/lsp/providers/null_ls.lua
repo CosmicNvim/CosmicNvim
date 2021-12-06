0f975eae2c48ba15a0156880662611cd817faf97
Matt Leong <mattleong91@gmail.com>
2021-12-06 09:20:11 -0800

    feat(lsp): add null_ls builtins

--- lua/cosmic/lsp/providers/null_ls.lua
+++ lua/cosmic/lsp/providers/null_ls.lua
@@ -6,6 +6,11 @@ local config_opts = config.lsp.servers.null_ls or {}
 null_ls.config(vim.tbl_deep_extend('force', {
   -- you must define at least one source for the plugin to work
   sources = {
+    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.stylua.with({
       args = {
         '-s',
@@ -17,16 +22,6 @@ null_ls.config(vim.tbl_deep_extend('force', {
         '2',
         '-',
       },
-    }),
-    null_ls.builtins.code_actions.gitsigns,
-    null_ls.builtins.diagnostics.eslint_d.with({ -- eslint or eslint_d
-      -- prefer_local = 'node_modules/.bin',
-    }),
-    null_ls.builtins.code_actions.eslint_d.with({ -- eslint or eslint_d
-      -- prefer_local = 'node_modules/.bin',
-    }),
-    null_ls.builtins.formatting.prettierd.with({ -- prettier, eslint, eslint_d, or prettierd
-      -- prefer_local = 'node_modules/.bin',
     }),
   },
 }, config_opts or {}))
