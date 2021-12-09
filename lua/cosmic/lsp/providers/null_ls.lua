11af995b7523dbf510c40e17715f0510393f3a30
Matt Leong <mattleong91@gmail.com>
2021-12-09 15:16:05 -0800

    feat(lsp): check for eslint config and package.json before starting eslint

--- lua/cosmic/lsp/providers/null_ls.lua
+++ lua/cosmic/lsp/providers/null_ls.lua
@@ -2,14 +2,30 @@ local config = require('cosmic.config')
 local defaults = require('cosmic.lsp.providers.defaults')
 local null_ls = require('null-ls')
 
+local has_eslint_config = function(utils)
+  return utils.root_has_file('.eslintrc')
+    or utils.root_has_file('.eslintrc.json')
+    or utils.root_has_file('.eslintrc.js')
+    or utils.root_has_file('package.json')
+    or utils.root_has_file('.eslintrc.cjs')
+    or utils.root_has_file('.eslintrc.yaml')
+    or utils.root_has_file('.eslintrc.yml')
+end
+
 local config_opts = config.lsp.servers.null_ls or {}
 null_ls.config(vim.tbl_deep_extend('force', {
   -- you must define at least one source for the plugin to work
   sources = {
-    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.code_actions.eslint_d.with({
+      condition = has_eslint_config,
+    }),
+    null_ls.builtins.diagnostics.eslint_d.with({
+      condition = has_eslint_config,
+    }),
+    null_ls.builtins.formatting.eslint_d.with({
+      condition = has_eslint_config,
+    }),
     null_ls.builtins.code_actions.gitsigns,
-    null_ls.builtins.diagnostics.eslint_d,
-    null_ls.builtins.formatting.eslint_d,
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.stylua,
   },
