e425ee84e5e3139f5eda16679a7e07bf243b175e
Matt Leong <mattleong91@gmail.com>
2021-12-13 08:58:48 -0800

    feat(lsp): update null ls settings

--- lua/cosmic/lsp/providers/null_ls.lua
+++ lua/cosmic/lsp/providers/null_ls.lua
@@ -1,5 +1,5 @@
-local config = require('cosmic.config')
 local defaults = require('cosmic.lsp.providers.defaults')
+local config = require('cosmic.config')
 local null_ls = require('null-ls')
 
 local has_eslint_config = function(utils)
@@ -13,7 +13,8 @@ local has_eslint_config = function(utils)
 end
 
 local config_opts = config.lsp.servers.null_ls or {}
-null_ls.config(vim.tbl_deep_extend('force', {
+
+require('null-ls').setup(vim.tbl_deep_extend('force', {
   -- you must define at least one source for the plugin to work
   sources = {
     null_ls.builtins.code_actions.eslint_d.with({
@@ -29,6 +30,6 @@ null_ls.config(vim.tbl_deep_extend('force', {
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.stylua,
   },
-}, config_opts or {}))
 
-require('lspconfig')['null-ls'].setup(vim.tbl_deep_extend('force', defaults, config_opts.opts or {}))
+  root_dir = defaults.root_dir,
+}, config_opts or {}))
