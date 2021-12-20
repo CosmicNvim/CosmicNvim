f522d0f3bf251ff19d133adf80e12bc0fe4154d8
Matt Leong <mattleong91@gmail.com>
2021-12-19 19:37:34 -0800

    feat: border config setting

--- lua/cosmic/config/examples/config.lua
+++ lua/cosmic/config/examples/config.lua
@@ -3,95 +3,97 @@
 -- You can require null-ls if needed
 -- local null_ls = require('null-ls')
 
-local config = {}
+local config = {
+  -- See :h cmp-usage
+  autocomplete = {},
 
--- default statusline icon
-config.statusline = {
-  main_icon = '★',
-}
+  -- See https://github.com/rmagatti/auto-session#%EF%B8%8F-configuration
+  auto_session = {},
 
--- theming, don't forget to run :CosmicReloadSync when changing themes
--- 'catppuccin'
--- 'dracula'
--- 'enfocado'
--- 'gruvbox'
--- 'nightfox'
--- 'nord'
--- 'onedark'
--- 'rose-pine'
-config.theme = 'tokyonight' -- don't define or set to nil, if you want to enable your own theme in cosmic/config/plugins.lua
+  -- See :h nvim_open_win for possible border options
+  border = 'rounded',
 
--- lsp settings
-config.lsp = {
+  -- https://github.com/numToStr/Comment.nvim#configuration-optional
+  comments = {},
 
-  -- true/false or table of filetypes {'.ts', '.js',}
-  format_on_save = true,
+  -- See https://github.com/CosmicNvim/cosmic-ui#%EF%B8%8F-configuration
+  cosmic_ui = {},
 
-  -- set to false to disable rename notification
-  rename_notification = true,
+  -- see :h vim.diagnostic.config for all diagnostic configuration options
+  diagnostic = {},
 
-  servers = {
+  -- lsp settings
+  lsp = {
 
-    -- enable non-default servers, use default lsp config
-    -- check here for configs that will be used by default: https://github.com/williamboman/nvim-lsp-installer/tree/main/lua/nvim-lsp-installer/servers
-    -- rust_analyzer = true,
+    -- true/false or table of filetypes {'.ts', '.js',}
+    format_on_save = true,
 
-    tsserver = {
-      -- disable formatting (defaults to true)
-      format = false,
-      -- OR add/override server options
-      opts = {
-        on_attach = function(client, bufnr) end,
-        flags = {
-          debounce_text_changes = 150,
+    -- set to false to disable rename notification
+    rename_notification = true,
+
+    servers = {
+
+      -- enable non-default servers, use default lsp config
+      -- check here for configs that will be used by default: https://github.com/williamboman/nvim-lsp-installer/tree/main/lua/nvim-lsp-installer/servers
+      -- rust_analyzer = true,
+
+      tsserver = {
+        -- disable formatting (defaults to true)
+        format = false,
+        -- OR add/override server options
+        opts = {
+          on_attach = function(client, bufnr) end,
+          flags = {
+            debounce_text_changes = 150,
+          },
         },
       },
+
+      -- See Cosmic defaults lsp/providers/null_ls.lua
+      null_ls = {},
     },
 
-    -- See Cosmic defaults lsp/providers/null_ls.lua
-    null_ls = {},
+    -- See Cosmic defaults lsp/providers/tsserver.lua
+    -- If adding additional sources, be sure to also copy the defaults that you would like to preserve from lsp/providers/null_ls.lua
+    ts_utils = {},
   },
 
-  -- See Cosmic defaults lsp/providers/tsserver.lua
-  -- If adding additional sources, be sure to also copy the defaults that you would like to preserve from lsp/providers/null_ls.lua
-  ts_utils = {},
+  -- See https://github.com/ray-x/lsp_signature.nvim#full-configuration-with-default-values
+  lsp_signature = {},
+
+  -- See https://github.com/L3MON4D3/LuaSnip/blob/577045e9adf325e58f690f4d4b4a293f3dcec1b3/README.md#config
+  luasnip = {},
+
+  -- default statusline icon
+  statusline = {
+    main_icon = '★',
+  },
+
+  -- See :h telescope.setup
+  telescope = {},
+
+  -- theming, don't forget to run :CosmicReloadSync when changing themes
+  -- 'catppuccin'
+  -- 'dracula'
+  -- 'enfocado'
+  -- 'gruvbox'
+  -- 'nightfox'
+  -- 'nord'
+  -- 'onedark'
+  -- 'rose-pine'
+  theme = 'tokyonight', -- don't define or set to nil, if you want to enable your own theme in cosmic/config/plugins.lua
+
+  -- See https://github.com/folke/todo-comments.nvim#%EF%B8%8F-configuration
+  todo_comments = {},
+
+  -- See :h nvim-treesitter-quickstart
+  treesitter = {},
+
+  -- See :h notify.setup
+  notify = {},
+
+  -- See :h nvim-tree.setup
+  nvim_tree = {},
 }
-
--- See :h cmp-usage
-config.autocomplete = {}
-
--- See https://github.com/rmagatti/auto-session#%EF%B8%8F-configuration
-config.auto_session = {}
-
--- https://github.com/numToStr/Comment.nvim#configuration-optional
-config.comments = {}
-
--- See https://github.com/CosmicNvim/cosmic-ui#%EF%B8%8F-configuration
-config.cosmic_ui = {
-  -- see :h vim.diagnostic.config for all diagnostic configuration options
-  -- see Cosmic-ui defaults: https://github.com/CosmicNvim/cosmic-ui/blob/main/lua/cosmic-ui/diagnostics/init.lua
-  diagnostic = {},
-}
-
--- See https://github.com/ray-x/lsp_signature.nvim#full-configuration-with-default-values
-config.lsp_signature = {}
-
--- See https://github.com/L3MON4D3/LuaSnip/blob/577045e9adf325e58f690f4d4b4a293f3dcec1b3/README.md#config
-config.luasnip = {}
-
--- See :h telescope.setup
-config.telescope = {}
-
--- See https://github.com/folke/todo-comments.nvim#%EF%B8%8F-configuration
-config.todo_comments = {}
-
--- See :h nvim-treesitter-quickstart
-config.treesitter = {}
-
--- See :h notify.setup
-config.notify = {}
-
--- See :h nvim-tree.setup
-config.nvim_tree = {}
 
 return config
