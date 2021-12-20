f5628829893fe04234dcd6a4619b7396a787dd52
Matt Leong <mattleong91@gmail.com>
2021-12-20 15:17:39 -0800

    feat(plugins): nvim-autopairs ts support

--- lua/cosmic/plugins/auto-pairs/init.lua
+++ lua/cosmic/plugins/auto-pairs/init.lua
@@ -1,4 +1,10 @@
 require('nvim-autopairs').setup({
+  check_ts = true,
+  ts_config = {
+    lua = { 'string', 'source' },
+    javascript = { 'string', 'template_string' },
+    java = false,
+  },
   disable_filetype = { 'TelescopePrompt', 'vim' },
 })
 
