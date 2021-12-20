68ff49da19d05f8be9a7881bfd46c0fd39d0f573
Matt Leong <mattleong91@gmail.com>
2021-12-19 18:00:18 -0800

    feat(cosmic-ui): no need to pass icons

--- lua/cosmic/plugins/cosmic-ui/init.lua
+++ lua/cosmic/plugins/cosmic-ui/init.lua
@@ -1,9 +1,7 @@
 local config = require('cosmic.config')
-local icons = require('cosmic.theme.icons')
 
 local defaults = {
   border = 'rounded',
-  icons = icons,
 }
 
 require('cosmic-ui').setup(vim.tbl_deep_extend('force', defaults, config.cosmic_ui or {}))
