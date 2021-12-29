54018cd5a45de5705c20847334b41c82e83bb6b7
Matt Leong <mattleong91@gmail.com>
2021-12-28 16:29:30 -0800

    fix(lsp): remove conditional for eslint

--- lua/cosmic/lsp/providers/null_ls.lua
+++ lua/cosmic/lsp/providers/null_ls.lua
@@ -3,29 +3,19 @@ local defaults = require('cosmic.lsp.providers.defaults')
 local config = require('cosmic.config')
 local null_ls = require('null-ls')
 
-local has_eslint_config = function(u)
-  return u.root_has_file('.eslintrc')
-    or u.root_has_file('.eslintrc.json')
-    or u.root_has_file('.eslintrc.js')
-    or u.root_has_file('package.json')
-    or u.root_has_file('.eslintrc.cjs')
-    or u.root_has_file('.eslintrc.yaml')
-    or u.root_has_file('.eslintrc.yml')
-end
-
 local config_opts = config.lsp.servers.null_ls or {}
 
 require('null-ls').setup(utils.merge({
   -- you must define at least one source for the plugin to work
   sources = {
     null_ls.builtins.code_actions.eslint_d.with({
-      condition = has_eslint_config,
+      prefer_local = 'node_modules/.bin',
     }),
     null_ls.builtins.diagnostics.eslint_d.with({
-      condition = has_eslint_config,
+      prefer_local = 'node_modules/.bin',
     }),
     null_ls.builtins.formatting.eslint_d.with({
-      condition = has_eslint_config,
+      prefer_local = 'node_modules/.bin',
     }),
     null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.formatting.prettier.with({
