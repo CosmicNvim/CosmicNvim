9680e81aa32907b001496c63be087637d1f92fd7
Matt Leong <mattleong91@gmail.com>
2022-01-17 14:34:41 -0800

    feat: delete mappings for tabs and buffers

--- lua/cosmic/core/mappings.lua
+++ lua/cosmic/core/mappings.lua
@@ -21,10 +21,12 @@ map('n', '<leader>cn', ':cnext<cr>zz')
 -- buffer navigation
 map('n', '<leader>bp', ':bprev<cr>')
 map('n', '<leader>bn', ':bnext<cr>')
+map('n', '<leader>bd', ':bdelete<cr>')
 
 -- tab navigation
 map('n', '<leader>tp', ':tabprevious<cr>')
 map('n', '<leader>tn', ':tabnext<cr>')
+map('n', '<leader>td', ':tabclose<cr>')
 
 -- resize with arrows
 map('n', '<C-Up>', ':resize -2<CR>')
