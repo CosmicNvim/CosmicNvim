a10143d8d26f19925bc52dd1e9bc712e82f8d433
Matt Leong <mattleong91@gmail.com>
2022-02-17 16:14:25 -0800

    fix: rm error when config.config.editor is not present

--- lua/cosmic/core/init.lua
+++ lua/cosmic/core/init.lua
@@ -9,7 +9,7 @@ local cosmic_modules = {
 
 for _, mod in ipairs(cosmic_modules) do
   local ok, err = pcall(require, mod)
-  if not ok and not mod:find('cosmic.core.user') then
+  if not ok and not mod:find('cosmic.config') then
     error(('Error loading %s...\n\n%s'):format(mod, err))
   end
 end
