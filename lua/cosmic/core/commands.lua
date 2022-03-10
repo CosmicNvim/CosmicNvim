7c1403438fcab8fbf5a3fa3fa58ca205eebd5ba0
Matt Leong <mattleong91@gmail.com>
2022-03-10 10:46:13 -0800

    feat(commands): use new autocmd api

--- lua/cosmic/core/commands.lua
+++ lua/cosmic/core/commands.lua
@@ -1,11 +1,13 @@
+local augroup_name = 'CosmicNvim'
+local group = vim.api.nvim_create_augroup(augroup_name, { clear = true })
+vim.api.nvim_create_autocmd('VimResized', {
+  command = 'tabdo wincmd =',
+  group = group,
+})
+
 vim.cmd([[
   command! CosmicUpdate lua require('cosmic.utils').update()
   command! CosmicReload lua require('cosmic.utils').reload_user_config(true)
   command! CosmicReloadSync lua require('cosmic.utils').reload_user_config_sync()
   command! LspFormat lua vim.lsp.buf.formatting()
-
-  augroup Cosmic_Auto_Resize
-    autocmd!
-    autocmd VimResized * tabdo wincmd =
-  augroup end
 ]])
