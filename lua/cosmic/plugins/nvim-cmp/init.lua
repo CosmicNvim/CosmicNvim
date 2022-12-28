120c30bbe0d7e7e911b8ca3dbc76b0b721bd71f3
Matthew Leong <mattleong91@gmail.com>
2022-12-28 15:29:29 -0800

    fix auto complete

--- lua/cosmic/plugins/nvim-cmp/init.lua
+++ lua/cosmic/plugins/nvim-cmp/init.lua
@@ -10,6 +10,7 @@ return {
     'saadparwaiz1/cmp_luasnip',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-nvim-lua',
+    'hrsh7th/cmp-cmdline',
     'hrsh7th/cmp-path',
     -- has configs
     'L3MON4D3/LuaSnip',
