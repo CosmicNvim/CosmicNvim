1e23745f643ca59629c7df961b8feed4704efd5c
Matthew Leong <mattleong91@gmail.com>
2023-01-27 10:27:41 -0800

    fix: user editor config error if not present

--- lua/cosmic/core/init.lua
+++ lua/cosmic/core/init.lua
@@ -24,5 +24,9 @@ vim.opt.runtimepath:prepend(lazypath)
 
 -- set up cosmicnvim
 for _, mod in ipairs(cosmic_modules) do
-  require(mod)
+  local ok, err = pcall(require, mod)
+  -- cosmic.config files may or may not be present
+  if not ok and not mod:find('cosmic.config') then
+    error(('Error loading %s...\n\n%s'):format(mod, err))
+  end
 end
