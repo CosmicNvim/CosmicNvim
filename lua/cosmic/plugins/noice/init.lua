527eff7c877a8c80960e2421faf7e8b385044458
Matthew Leong <mattleong91@gmail.com>
2023-08-24 07:15:47 -0700

    feat: pin back to v0.9.0

--- lua/cosmic/plugins/noice/init.lua
+++ lua/cosmic/plugins/noice/init.lua
@@ -8,6 +8,7 @@ return {
     local config = u.merge({
       presets = {
         lsp_doc_border = true,
+        long_message_to_split = true,
       },
       views = {
         notify = {
@@ -25,7 +26,7 @@ return {
         override = {
           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
           ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = false,
+          ['cmp.entry.get_documentation'] = true,
         },
       },
     }, user_config.plugins.noice or {})
