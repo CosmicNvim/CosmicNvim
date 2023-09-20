b42b3642be2f551247223879cd2196a5b30b5e8d
Matthew Leong <mattleong91@gmail.com>
2023-09-19 19:09:28 -0700

    feat: update lazy lock + swap prettierd with prettier

--- lua/cosmic/plugins/null-ls/init.lua
+++ lua/cosmic/plugins/null-ls/init.lua
@@ -19,17 +19,13 @@ return {
       end
 
       config_opts.sources = u.merge_list({
+        null_ls.builtins.code_actions.gitsigns,
         null_ls.builtins.code_actions.eslint_d,
         null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.formatting.eslint_d,
         null_ls.builtins.diagnostics.markdownlint,
-        null_ls.builtins.formatting.prettierd.with({
-          env = {
-            PRETTIERD_LOCAL_PRETTIER_ONLY = 1,
-          },
-        }),
+        null_ls.builtins.formatting.eslint_d,
+        null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.code_actions.gitsigns,
       }, get_user_config_sources())
     end
 
