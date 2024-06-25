1de6f108531a88f9732c0a74a6998c5b82a3ff03
Matthew Leong <mattleong91@gmail.com>
2024-06-25 10:21:37 -0700

    feat: update gitsigns setup

--- lua/cosmic/plugins/gitsigns/init.lua
+++ lua/cosmic/plugins/gitsigns/init.lua
@@ -9,11 +9,11 @@ return {
     local gs = require('gitsigns')
     gs.setup(u.merge({
       signs = {
-        add = { hl = 'GitSignsAdd', text = '│', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-        change = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-        delete = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        topdelete = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+        add = { text = '│' },
+        change = { text = '│' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
       },
       preview_config = {
         -- Options passed to nvim_open_win
