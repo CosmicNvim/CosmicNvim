7b3d76ff30c3adae1ce8df28c87b43754aeede82
Matthew Leong <mattleong91@gmail.com>
2025-02-03 11:22:03 -0800

    feat: update python path for poetry

--- lua/cosmic/lsp/servers/pyright.lua
+++ lua/cosmic/lsp/servers/pyright.lua
@@ -1,21 +1,58 @@
-local path = require('lspconfig/util').path
+local util = require('lspconfig/util')
 
-local function get_python_path(workspace)
-  -- Use activated virtualenv.
+local function prefer_bin_from_venv(executable_name)
+  -- Return the path to the executable if $VIRTUAL_ENV is set and the binary exists somewhere beneath the $VIRTUAL_ENV path, otherwise get it from Mason
   if vim.env.VIRTUAL_ENV then
-    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
-  end
-
-  -- Find and use virtualenv in workspace directory.
-  for _, pattern in ipairs({ '*', '.*' }) do
-    local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
-    if match ~= '' then
-      return path.join(path.dirname(match), 'bin', 'python')
+    local paths = vim.fn.glob(vim.env.VIRTUAL_ENV .. '/**/bin/' .. executable_name, true, true)
+    local executable_path = table.concat(paths, ', ')
+    if executable_path ~= '' then
+      --vim.api.nvim_echo({ { "Using path for " .. executable_name .. ": " .. executable_path, "None" } }, false, {})
+      return executable_path
     end
   end
 
-  -- Fallback to system Python.
-  return exepath('python3') or exepath('python') or 'python'
+  -- find poetry.lock file in current directory or parent directories
+  local poetry_lock = vim.fn.findfile('poetry.lock', '.;')
+  if poetry_lock ~= '' then
+    -- use `poetry env info -p -C <path of folder containing poetry.lock>` to get the virtualenv path
+    local poetry_env_path =
+      vim.fn.systemlist('poetry env list --full-path -C ' .. vim.fn.fnamemodify(poetry_lock, ':h'))
+
+    if #poetry_env_path > 0 then
+      local candidate_paths = {}
+      for _, env_path in ipairs(poetry_env_path) do
+        if env_path ~= '' then
+          if string.sub(env_path, 0, 1) == '/' then
+            table.insert(candidate_paths, 0, env_path)
+          end
+        end
+      end
+      local selected_poetry_env_path = candidate_paths[0]
+      if #poetry_env_path > 1 then
+        for _, env_path in ipairs(candidate_paths) do
+          if string.find(env_path, 'Activated') then
+            -- Must strip the word "(Activated)" from the string
+
+            selected_poetry_env_path = string.gsub(env_path, ' %(Activated%)', '')
+            selected_poetry_env_path = vim.fn.trim(selected_poetry_env_path)
+            break
+          end
+        end
+      end
+      local venv_path_to_python = selected_poetry_env_path .. '/bin/' .. executable_name
+      if vim.fn.filereadable(venv_path_to_python) == 1 then
+        return venv_path_to_python
+      end
+    end
+  end
+
+  local mason_registry = require('mason-registry')
+  if mason_registry.has_package(executable_name) == false then
+    return executable_name
+  end
+  local mason_path = mason_registry.get_package(executable_name):get_install_path() .. '/venv/bin/' .. executable_name
+  -- vim.api.nvim_echo({ { "Using path for " .. executable_name .. ": " .. mason_path, "None" } }, false, {})
+  return mason_path
 end
 
 return {
@@ -23,12 +60,11 @@ return {
     python = {
       analysis = {
         typeCheckingMode = 'off',
+        diagnosticMode = 'off',
         useLibraryCodeForTypes = true,
         completeFunctionParens = true,
       },
+      pythonPath = prefer_bin_from_venv('python'),
     },
   },
-  before_init = function(_, config)
-    config.settings.python.pythonPath = get_python_path(config.root_dir)
-  end,
 }
