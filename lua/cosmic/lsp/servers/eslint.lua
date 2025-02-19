c60899c09b13604d730df33ff6705531fcb46f83
Matthew Leong <mattleong91@gmail.com>
2025-02-19 08:31:14 -0800

    feat: add eslint flags

--- lua/cosmic/lsp/servers/eslint.lua
+++ lua/cosmic/lsp/servers/eslint.lua
@@ -1,6 +1,11 @@
 local defaults = require('cosmic.lsp.servers.defaults')
 local can_format_on_save = require('cosmic.utils.lsp').can_client_format_on_save
 return {
+  flags = {
+    allow_incremental_sync = false,
+    debounce_text_changes = 1000,
+    exit_timeout = 1500,
+  },
   on_attach = function(client, bufnr)
     defaults.on_attach(client, bufnr)
     if can_format_on_save(client) then
