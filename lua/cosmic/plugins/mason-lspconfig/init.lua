7c7cdd60e74992b4dff3c929de3c9942206a798f
Matthew Leong <mattleong91@gmail.com>
2025-11-24 08:13:08 -0800

    feat: clean up lsp init

--- lua/cosmic/plugins/mason-lspconfig/init.lua
+++ lua/cosmic/plugins/mason-lspconfig/init.lua
@@ -8,36 +8,28 @@ return {
     local u = require('cosmic.utils')
     local default_config = require('cosmic.lsp.servers.defaults')
 
-    local start_server = function(server)
-      local server_config = default_config
-
-      -- set up default cosmic options
-      local ok, cosmic_server_config = pcall(require, 'cosmic.lsp.servers.' .. server)
-      if ok then
-        server_config = u.merge(server_config, cosmic_server_config)
-      end
-
-      -- override options if user defines them
-      if type(user_config.lsp.servers[server]) == 'table' and user_config.lsp.servers[server].opts ~= nil then
-        server_config = u.merge(server_config, user_config.lsp.servers[server].opts)
-      end
-
-      vim.lsp.config(server, server_config)
-      vim.lsp.enable(server)
-    end
-
-    for config_server, config_opt in pairs(user_config.lsp.servers) do
+    for user_server, config_opt in pairs(user_config.lsp.servers) do
       if not config_opt == false then
-        start_server(config_server)
+        local server_config = default_config
+
+        -- set up default cosmic options
+        local ok, cosmic_server_config = pcall(require, 'cosmic.lsp.servers.' .. user_server)
+        if ok then
+          server_config = u.merge(server_config, cosmic_server_config)
+        end
+
+        -- override options if user defines them
+        if type(user_config.lsp.servers[user_server]) == 'table' and user_config.lsp.servers[user_server].opts ~= nil then
+          server_config = u.merge(server_config, user_config.lsp.servers[user_server].opts)
+        end
+
+        vim.lsp.config(user_server, server_config)
+        vim.lsp.enable(user_server)
       end
     end
   end,
   dependencies = {
-    { 'neovim/nvim-lspconfig', lazy = true },
-    {
-      'williamboman/mason.nvim',
-      opts = {
-      },
-    },
+    { 'neovim/nvim-lspconfig',  lazy = true },
+    { 'williamboman/mason.nvim' , opts = {}},
   },
 }
