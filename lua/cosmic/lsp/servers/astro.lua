8787b66f189d026dd5d493130e8c8400b2b21656
Matthew Leong <mattleong91@gmail.com>
2026-02-16 22:44:39 -0800

    fix: python path + tailwindcss

---
+++ lua/cosmic/lsp/servers/astro.lua
@@ -0,0 +1,4 @@
+---@diagnostic disable: missing-fields
+---@type vim.lsp.ClientConfig
+return {
+}
--- lua/cosmic/lsp/servers/tailwindcss.lua
+++ lua/cosmic/lsp/servers/tailwindcss.lua
@@ -1,9 +1,9 @@
 ---@diagnostic disable: missing-fields
 ---@type vim.lsp.ClientConfig
 return {
-  root_dir = function(fname)
-    return require('lspconfig').util.root_pattern('tailwind.config.cjs', 'tailwind.config.js', 'tailwind.config.ts')(
-      fname
-    )
-  end,
+  --[[ root_dir = function(fname) ]]
+  --[[   return require('lspconfig').util.root_pattern('tailwind.config.cjs', 'tailwind.config.js', 'tailwind.config.ts')( ]]
+  --[[     fname ]]
+  --[[   ) ]]
+  --[[ end, ]]
 }
