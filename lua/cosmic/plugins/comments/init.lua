0989308bee32337b8ec0ed1a1b88a43c369e5a58
Matthew Leong <mattleong91@gmail.com>
2026-03-30 11:03:07 -0700

    refactor(comments): extract commentstring hook helpers
    Move the pre-hook logic into top-level helpers and keep the plugin opts
    declaration minimal.

--- lua/cosmic/plugins/comments/init.lua
+++ lua/cosmic/plugins/comments/init.lua
@@ -1,3 +1,68 @@
+local function get_line_start_location(line)
+  local first_non_whitespace_col = vim.fn.match(vim.fn.getline(line + 1), '\\S')
+
+  return {
+    line,
+    math.max(first_non_whitespace_col, 0),
+  }
+end
+
+local function is_visual_comment(ctx)
+  local comment_utils = require('Comment.utils')
+
+  return ctx.cmotion == comment_utils.cmotion.block
+    or ctx.cmotion == comment_utils.cmotion.v
+    or ctx.cmotion == comment_utils.cmotion.V
+end
+
+local function get_comment_location(ctx)
+  if is_visual_comment(ctx) then
+    return get_line_start_location(vim.fn.getpos("'<")[2] - 1)
+  end
+
+  return get_line_start_location(vim.api.nvim_win_get_cursor(0)[1] - 1)
+end
+
+local function find_jsx_ancestor(node)
+  while node do
+    local node_type = node:type()
+
+    if node_type == 'jsx_element' or node_type == 'jsx_fragment' or node_type == 'jsx_self_closing_element' then
+      return node
+    end
+
+    node = node:parent()
+  end
+end
+
+local function get_commentstring(ctx)
+  local comment_utils = require('Comment.utils')
+  local ts_context_commentstring = require('ts_context_commentstring')
+  local key = ctx.ctype == comment_utils.ctype.linewise and '__default' or '__multiline'
+  local location = get_comment_location(ctx)
+  local ok, parser = pcall(vim.treesitter.get_parser, 0)
+
+  if ok then
+    parser:parse(true)
+
+    local node = parser:named_node_for_range({
+      location[1],
+      location[2],
+      location[1],
+      location[2],
+    })
+
+    if find_jsx_ancestor(node) then
+      return '{/* %s */}'
+    end
+  end
+
+  return ts_context_commentstring.calculate_commentstring({
+    key = key,
+    location = location,
+  })
+end
+
 return {
   'numToStr/Comment.nvim',
   dependencies = {
@@ -30,70 +95,8 @@ return {
       },
     },
   },
-  opts = function()
-    local comment_utils = require('Comment.utils')
-    local ts_context_commentstring = require('ts_context_commentstring')
-
-    local function get_line_start_location(line)
-      local first_non_whitespace_col = vim.fn.match(vim.fn.getline(line + 1), '\\S')
-
-      return {
-        line,
-        math.max(first_non_whitespace_col, 0),
-      }
-    end
-
-    local function get_comment_location(ctx)
-      if
-        ctx.cmotion == comment_utils.cmotion.block
-        or ctx.cmotion == comment_utils.cmotion.v
-        or ctx.cmotion == comment_utils.cmotion.V
-      then
-        return get_line_start_location(vim.fn.getpos("'<")[2] - 1)
-      end
-
-      return get_line_start_location(vim.api.nvim_win_get_cursor(0)[1] - 1)
-    end
-
-    local function find_jsx_ancestor(node)
-      while node do
-        local node_type = node:type()
-
-        if node_type == 'jsx_element' or node_type == 'jsx_fragment' or node_type == 'jsx_self_closing_element' then
-          return node
-        end
-
-        node = node:parent()
-      end
-    end
-
-    return {
-      pre_hook = function(ctx)
-        local key = ctx.ctype == comment_utils.ctype.linewise and '__default' or '__multiline'
-        local ok, parser = pcall(vim.treesitter.get_parser, 0)
-        local location = get_comment_location(ctx)
-
-        if ok then
-          parser:parse(true)
-
-          local node = parser:named_node_for_range({
-            location[1],
-            location[2],
-            location[1],
-            location[2],
-          })
-
-          if find_jsx_ancestor(node) then
-            return '{/* %s */}'
-          end
-        end
-
-        return ts_context_commentstring.calculate_commentstring({
-          key = key,
-          location = location,
-        })
-      end,
-    }
-  end,
+  opts = {
+    pre_hook = get_commentstring,
+  },
   lazy = false,
 }
